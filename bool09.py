9213a97cc314b3a1f42499e7ebf80176412f51e4
Roma1125 <114662266+Roma1125@users.noreply.github.com>
2022-10-09 21:35:19 +0500

    csaaasaasxa
    acacacacsacasc

--- bool09.py
+++ bool09.py
@@ -1,10 +1,11 @@
 def main(a):
-    """Check the natural number.Natural numbers are numbers used in counting.
+    """
+    Check the logic "The variable 'a' is equal 7"
     Args:
         a: int
     Returns:
         bool
     """
     # Write your code here
-    return (a>0)
-print(main(-3))
+    return (int(a)-float(a)==0 and a>0)
+print(main((1.2))) 
